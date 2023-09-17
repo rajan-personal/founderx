997853c59263a3725efa868ecd14f9fb07529998
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2023-09-17 14:13:53 +0530

    test deploy

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ flutter build web --release
 cp -r build/web/* ../docs
 cd ..
 cd docs
-sed -i '' 's|<base href="/">|<base href="https://founderx.mysave.app/">|g' index.html
+sed -i '' 's|<base href="/">|<base href="https://founderx.mysave.app">|g' index.html
 echo "founderx.mysave.app" >> CNAME
