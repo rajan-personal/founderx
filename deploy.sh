648bb60a01a954ced6a6eb1b0bcc2832d220e987
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2023-09-17 14:04:07 +0530

    url fix

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 rm -rf docs
 mkdir docs
 cd founderx
-flutter build web --release --base-href "https://founderx.mysave.app/"
+flutter build web --release
 cp -r build/web/* ../docs
 cd ..
 cd docs
+sed -i '' 's|<base href="/">|<base href="https://founderx.mysave.app/">|g' index.html
 echo "founderx.mysave.app" >> CNAME
